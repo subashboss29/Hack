9725fe28009baed0302a3fb8d2cc6ad6866eb6a5
subash.lingapandi <subash.lingapandi@INDIA-SubashL.perficient.com>
2016-07-25 23:43:33 +0530

    First program skeleton

--- HackerRank/src/Euler1.java
+++ HackerRank/src/Euler1.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class Euler1 {
+	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);	
+		int totalLoop = sc.nextInt();
+		
+		int[] testcaselist = new int[totalLoop];
+		
+		// Loop Start
+		for(int i= 0; i < totalLoop; i++){
+			testcaselist[i] = sc.nextInt(); 
+		}
+		
+		for(int j=0; j < testcaselist.length; j++){
+			System.out.println(testcaselist[j]);
+		}
+	}
 
 }
